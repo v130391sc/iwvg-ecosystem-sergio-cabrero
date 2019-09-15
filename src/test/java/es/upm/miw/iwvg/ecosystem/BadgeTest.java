b0f99e5fe65bdb4d3e184fb25ac9d9bb58620f29
Sergio Cabrero <s.cabrerof4@gmail.com>
2019-09-15 19:32:08 +0200

    initialize

---
+++ src/test/java/es/upm/miw/iwvg/ecosystem/BadgeTest.java
@@ -0,0 +1,17 @@
+package es.upm.miw.iwvg.ecosystem;
+
+import es.upm.miw.iwvg.ecosystem.api.Badge;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class BadgeTest {
+
+    @Test
+    void testGenerateBadge() {
+        String badge = new Badge().generateBadge("Heroku", "v2.2.0-SNAPSHOT");
+        assertNotNull(badge);
+        assertEquals("<svg", badge.substring(0, 4));
+    }
+}
