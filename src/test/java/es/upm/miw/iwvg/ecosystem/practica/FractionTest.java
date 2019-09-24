e0d9203e1471fdc9a462e17987a07203b9451202
Sergio Cabrero Fernandez <sergio.cabrero@neoris.com>
2019-09-24 10:58:33 +0200

    Add Fraction + Test #12

---
+++ src/test/java/es/upm/miw/iwvg/ecosystem/practica/FractionTest.java
@@ -0,0 +1,35 @@
+package es.upm.miw.iwvg.ecosystem.practica;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FractionTest {
+    Fraction fraction;
+
+    @BeforeEach
+    void before() {
+        fraction = new Fraction(4, 2);
+    }
+
+    @Test
+    void testFractionIntInt() {
+        assertEquals(4, fraction.getNumerator());
+        assertEquals(2, fraction.getDenominator());
+    }
+
+    @Test
+    void testPoint() {
+        fraction = new Fraction();
+        assertEquals(1, fraction.getNumerator());
+        assertEquals(1, fraction.getDenominator());
+    }
+
+    @Test
+    void testDecimal(){
+        fraction = new Fraction(3,2);
+        assertEquals(1.5, fraction.decimal());
+    }
+
+}
